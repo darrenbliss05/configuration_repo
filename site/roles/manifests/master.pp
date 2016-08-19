dbbe387bee91955d8eea91153d7239a0f7fa3831
puppet <debliss1@gmail.com>
2016-08-18 21:07:17 -0400

    Create generic role called master

---
+++ site/roles/manifests/master.pp
@@ -0,0 +1,6 @@
+class roles::master() {
+# Here we include all of the profiles that make up a Puppet master
+# for now it’s just the base (the SOE) but we will probably add more later
+include profiles::base
+}
+
