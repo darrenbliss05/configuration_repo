5cc007d5b313de1cc1b6b64bb973e56adb274de7
puppet <debliss1@gmail.com>
2016-08-18 21:10:20 -0400

    updated roles file

--- site/roles/manifests/jenkins.pp
+++ site/roles/manifests/jenkins.pp
@@ -1,7 +1,7 @@
-class profiles::jenkins {
-
-  $version = hiera('jenkins::version','latest')
-  class {'jenkins':
-     version => $version,
-  }
+class roles::jenkins() {
+# Here we include all of the profiles that make up a Puppet master
+# for now it’s just the base (the SOE) but we will probably add more later
+include profiles::base
+include profiles::jenkins
 }
+
