0c32b50314e947da3a8bc089ffeb2590501a6fa3
puppet <debliss1@gmail.com>
2016-08-18 20:55:20 -0400

    created inital base profile template

---
+++ site/profiles/manifests/base.pp
@@ -0,0 +1,12 @@
+class profiles::base {
+#the base profile should include component modules that will be on all nodes
+accounts::user { 'monitoring':
+ ensure => present,
+ uid => '450',
+ gid => '450',
+ managehome => false,
+ locked => true,
+}
+
+}
+
